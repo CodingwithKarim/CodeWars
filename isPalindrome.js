f07719e5e2c5f791c6b6e7d20ec6a7c5cd19a55a
Karim Hassan <karimhassan92@gmail.com>
2021-09-17 03:46:32 -0400

    code

---
+++ isPalindrome.js
@@ -0,0 +1,6 @@
+function isPalindrome(str){
+  str = str.toLowerCase();
+  for (let i = 0; i <= (str.length / 2); i++){
+  if (str[i] != str[str.length - (i + 1)]) return false;
+}
+return true;}
---
+++ twiceAsOld.js
@@ -0,0 +1,8 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  if ((sonYearsOld * 2) >= dadYearsOld){
+  return (2 * sonYearsOld) - dadYearsOld 
+}
+  else if ((sonYearsOld * 2) <= dadYearsOld){
+    return dadYearsOld - (2 * sonYearsOld)
+  }
+}
